15df0de32f3a378c63faef3ae661f51e05b7a1c8
mstonya2023 <latonya.davis08@gmail.com>
2023-09-08 17:08:18 -0700

    Connect to Database

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+// connect to the database with AFTER the config vars are processed
+require('./config/database');
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var imeetRouter = require('./routes/imeet');
 
 var app = express();
 
@@ -20,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/imeet', imeetRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
