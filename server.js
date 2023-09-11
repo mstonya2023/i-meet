f4a1e78dffc2b6c89f40a49a7631c4ec430e0692
mstonya2023 <latonya.davis08@gmail.com>
2023-09-11 08:34:06 -0700

    Install method-override

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('method'));
 
 app.use('/', indexRouter);
 app.use('/imeet', imeetRouter);
